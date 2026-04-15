27e08de6af2bce589defd9c62b6d016a928e1b96
Juman <jumankalita967@gmail.com>
2026-04-16 02:32:15 +0530

    fix: clean up duplicate route stubs from router plugin

--- src/routes/_app.tracker.tsx
+++ src/routes/_app.tracker.tsx
@@ -1,15 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router';
+﻿import { createFileRoute } from '@tanstack/react-router';
 import { SharedTrackerPage } from '@/components/pages/SharedTrackerPage';
 
 export const Route = createFileRoute('/_app/tracker')({
   component: SharedTrackerPage,
 });
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_app/tracker')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_app/tracker"!</div>
-}
