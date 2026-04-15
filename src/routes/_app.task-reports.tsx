369267cdb6ef82e257572235cf9dad4384591f22
Juman <jumankalita967@gmail.com>
2026-04-16 03:28:34 +0530

    fix: clean task-reports route stub

--- src/routes/_app.task-reports.tsx
+++ src/routes/_app.task-reports.tsx
@@ -1,15 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router';
+﻿import { createFileRoute } from '@tanstack/react-router';
 import { TaskReportsPage } from '@/components/pages/TaskReportsPage';
 
 export const Route = createFileRoute('/_app/task-reports')({
   component: TaskReportsPage,
 });
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_app/task-reports')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_app/task-reports"!</div>
-}
